62aea7558662194b8ac6c013d1826a2f612e86da
Splatman86 <120541361+Splatman86@users.noreply.github.com>
2024-03-14 20:39:32 -0700

    RYAN NO

--- Perish The Parasites/Scenes/Game Over.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Prototype.tscn")
-	Global.LegParasites = 0
-	Global.TorsoParasites = 0
-	Global.HeadParasites = 0
